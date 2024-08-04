45c0de8abd50079807e97932e9dda606f8ab8136
Usov <usov.lesha2017@gmail.com>
2024-08-04 21:47:33 +0300

    fix subject and create migration

--- domain/entities/Subject.cs
+++ domain/entities/Subject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,14 @@ namespace domain.entities
 {
     public class Subject
     {
+        [Key]
         public int id { get; set; }
         public string name{ get; set; }
         public string description { get; set; }
         public int needHours { get; set; }
         public int authorId { get; set; }
+
+
         public Teacher author { get; set; }
 
         public List<FollowStudentSubject> followStudentSubjects { get; set; }
